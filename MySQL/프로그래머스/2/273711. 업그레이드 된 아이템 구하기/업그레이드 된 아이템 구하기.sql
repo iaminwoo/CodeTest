a1e166eb2142858099b098799df3726bcff3dee2
inwoo <dlsdn1108@gmail.com>
2024-12-03 12:39:56 +0900

    Update 업그레이드 된 아이템 구하기.sql

--- MySQL/프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ MySQL/프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -3,10 +3,10 @@ select info.ITEM_ID as ITEM_ID, info.ITEM_NAME as ITEM_NAME,
 info.RARITY as RARITY
 from ITEM_INFO as info
 inner join (
-select tree.ITEM_ID
-from ITEM_INFO as info inner join ITEM_TREE as tree
-on info.ITEM_ID = tree.PARENT_ITEM_ID
-where info.RARITY = 'RARE'
+  select tree.ITEM_ID
+  from ITEM_INFO as info inner join ITEM_TREE as tree
+  on info.ITEM_ID = tree.PARENT_ITEM_ID
+  where info.RARITY = 'RARE'
 ) as up
 on info.ITEM_ID = up.ITEM_ID
 order by info.ITEM_ID desc;
