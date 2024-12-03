4d5e3ceb3fb5bb1f5feea68c29c12a44778a3b48
inwoo <dlsdn1108@gmail.com>
2024-12-03 14:32:16 +0900

    조건에 맞는 개발자 찾기.sql 리펙토링

--- MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,12 +1,9 @@
 -- 코드를 작성해주세요
-select ID, EMAIL, FIRST_NAME, LAST_NAME
-from DEVELOPERS
-where (
-    DEVELOPERS.SKILL_CODE & (select `CODE` from SKILLCODES where NAME = 'Python')
-    = (select `CODE` from SKILLCODES where NAME = 'Python')
-)
-or (
-    DEVELOPERS.SKILL_CODE & (select `CODE` from SKILLCODES where NAME = 'C#')
-    = (select `CODE` from SKILLCODES where NAME = 'C#')
-)
-order by ID asc;
+ELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE DEVELOPERS.SKILL_CODE & (
+    SELECT SUM(CODE) 
+    FROM SKILLCODES 
+    WHERE NAME IN ('Python', 'C#')
+) > 0
+ORDER BY ID ASC;
