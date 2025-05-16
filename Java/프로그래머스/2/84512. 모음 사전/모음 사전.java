d60bb462affc5cf22a2938a2b270a3e5dd3a29bd
inwoo <dlsdn1108@gmail.com>
2025-05-16 20:58:18 +0900

    [level 2] Title: 모음 사전, Time: 20.14 ms, Memory: 91.4 MB -BaekjoonHub

--- Java/프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ Java/프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,21 +1,28 @@
-import java.util.*;
-
 public class Solution {
     public int solution(String s) {
-        generate("");
-        return array.indexOf(s);
+        count = 0;
+        found = false;
+        count("", s);
+        return count;
     }
 
-    private final List<String> array = new ArrayList<>();
+    private int count;
     private final char[] vowels = new char[] {'A', 'E', 'I', 'O', 'U'};
+    private boolean found;
 
-    private void generate(String s) {
-        array.add(s);
+    private void count(String word, String s) {
+        if(s.equals(word)) {
+            found = true;
+            return;
+        }
 
-        if(s.length() == 5) return;
+        if(found) return;
+        count++;
+
+        if(word.length() == 5) return;
 
         for(char c : vowels) {
-            generate(s + c);
+            count(word + c, s);
         }
     }
 }
