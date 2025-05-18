d0f4245f0aad95cab06b0bf9a4a2da3080e8cdc7
inwoo <dlsdn1108@gmail.com>
2025-05-18 12:00:17 +0900

    Update 소수 찾기.java

--- Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,40 +3,33 @@ import java.util.*;
 public class Solution {
     public int solution(String numbers) {
         List<String> list = List.of(numbers.split(""));
+        int count = 0;
+        final Set<Integer> numSet = new HashSet<>();
 
         for(String s : list) {
             List<String> numList = new ArrayList<>(List.copyOf(list));
             numList.remove(s);
-            check(s, numList);
+            check(s, numList, numSet);
         }
 
-        System.out.println(numSet);
-        for(String s : numSet) {
+        for(int s : numSet) {
             if(isPrime(s)) count++;
         }
 
         return count;
     }
 
-    private int count;
-    private final Set<String> numSet = new HashSet<>();
-
-    private void check(String s, List<String> list) {
-        numSet.add(s);
+    private void check(String s, List<String> list, Set<Integer> numSet) {
+        numSet.add(Integer.parseInt(s));
 
         for(String num : list) {
             List<String> numList = new ArrayList<>(List.copyOf(list));
             numList.remove(num);
-            if("0".equals(s)) {
-                check(num, numList);
-            } else {
-                check(s + num, numList);
-            }
+            check(s + num, numList, numSet);
         }
     }
 
-    private boolean isPrime(String s) {
-        int num = Integer.parseInt(s);
+    private boolean isPrime(int num) {
         if(num <= 1) return false;
         for(int i = 2 ; (i * i) <= num ; i++){
             if(num % i == 0) return false;
