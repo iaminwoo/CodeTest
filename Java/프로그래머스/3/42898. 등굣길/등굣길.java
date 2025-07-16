1c41f8763e1fe8e72095e06dd7bed5d9bd723dc4
inwoo <dlsdn1108@gmail.com>
2025-07-16 09:03:14 +0900

    [level 3] Title: 등굣길, Time: 1.79 ms, Memory: 54.2 MB -BaekjoonHub

--- Java/프로그래머스/3/42898. 등굣길/등굣길.java
+++ Java/프로그래머스/3/42898. 등굣길/등굣길.java
@@ -8,24 +8,23 @@ class Solution {
             Arrays.fill(row, -1);
         }
         
-        return move(m, n, 1, 1, puddles);
+        boolean[][] isPuddle = new boolean[n + 1][m + 1];
+        
+        for(int[] puddle : puddles) {
+            isPuddle[puddle[1]][puddle[0]] = true;
+        }
+            
+        return move(m, n, 1, 1, isPuddle);
     }
     
-    private int move(int m, int n, int y, int x, int[][] puddles) {
+    private int move(int m, int n, int y, int x, boolean[][] isPuddle) {
         if(mem[y][x] != -1) return mem[y][x];
         
         if(x == m && y == n) return 1;
         
-        int right = (x == m) || isPuddle(y, x + 1, puddles) ? 0 : move(m, n, y, x + 1, puddles);
-        int down = (y == n) || isPuddle(y + 1, x, puddles) ? 0 : move(m, n, y + 1, x, puddles);
+        int right = (x == m) || isPuddle[y][x + 1] ? 0 : move(m, n, y, x + 1, isPuddle);
+        int down = (y == n) || isPuddle[y + 1][x] ? 0 : move(m, n, y + 1, x, isPuddle);
         
         return mem[y][x] = (right + down) % 1000000007;
-    }
-    
-    private boolean isPuddle(int y, int x, int[][] puddles) {
-        for(int[] puddle : puddles) {
-            if(puddle[0] == x && puddle[1] == y) return true;
-        }
-        return false;
     }
 }
