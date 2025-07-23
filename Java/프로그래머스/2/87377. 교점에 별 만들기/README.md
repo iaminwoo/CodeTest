4920ef110eabdfafbde33f7417f546743165fd6a
inwoo <dlsdn1108@gmail.com>
2025-07-23 23:12:59 +0900

    [level 2] Title: 교점에 별 만들기, Time: 24.53 ms, Memory: 84.4 MB -BaekjoonHub

--- Java/프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ Java/프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
 
 ### 성능 요약
 
-메모리: 93.7 MB, 시간: 26.70 ms
+메모리: 84.4 MB, 시간: 24.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 10일 23:28:33
+2025년 07월 23일 23:12:56
 
 ### 문제 설명
 
--- Java/프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ Java/프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,76 +1,72 @@
 import java.util.*;
 
-public class Solution {
-    public String[] solution(int[][] line) {
-        // 1. 두 선의 교점, 정수 체크
-        List<Point> points = new ArrayList<>();
-        for (int i = 0 ; i < line.length ; i++) {
-            for(int j = i + 1 ; j < line.length ; j++) {
-                Point point = intersection(line[i], line[j]);
-                if (Objects.nonNull(point)) {
-                    points.add(point);
-                }
-            }
-        }
+class Solution {
+    private static class Point {
+        private final int x;
+        private final int y;
 
-        // 2. 너비, 높이 계산
-        long[] minMax = getXYMinMax(points);
-        long xMax = minMax[0];
-        long xMin = minMax[2];
-        long yMax = minMax[1];
-        long yMin = minMax[3];
-
-        int width = (int) (xMax - xMin + 1);
-        int height = (int) (yMax - yMin + 1);
-        char[][] stars = new char[height][width];
-        for(char[] ca : stars) {
-            Arrays.fill(ca, '.');
-        }
-
-        // 3. 별 찍기
-        for(Point p : points) {
-            stars[(int) (yMax - p.y)][(int) (p.x - xMin)] = '*';
-        }
-
-        // 4. 결과 반환
-        return Arrays.stream(stars)
-                .map(String::new)
-                .toArray(String[]::new);
-    }
-
-    private static class Point{
-        final long x;
-        final long y;
-
-        private Point(long x, long y) {
+        private Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
+    
+    private Point getPoint(int[] lineA, int[] lineB) {
+        long xUp  = (long)lineA[1] * lineB[2] - (long)lineA[2] * lineB[1];
+        long yUp  = (long)lineA[2] * lineB[0] - (long)lineA[0] * lineB[2];
+        long down = (long)lineA[0] * lineB[1] - (long)lineA[1] * lineB[0];
 
-    private Point intersection(int[] lineA, int[] lineB) {
-        long den = (long) lineA[0] * lineB[1] - (long) lineA[1] * lineB[0];
-        long xNum = (long) lineA[1] * lineB[2] - (long) lineA[2] * lineB[1];
-        long yNum = (long) lineA[2] * lineB[0] - (long) lineA[0] * lineB[2];
+        if (down == 0) return null;
 
-        if(den == 0 || xNum % den != 0 || yNum % den != 0) return null;
-
-        return new Point(xNum / den, yNum / den);
-    }
-
-    private long[] getXYMinMax(List<Point> points) {
-        long xMax = Long.MIN_VALUE;
-        long yMax = Long.MIN_VALUE;
-        long xMin = Long.MAX_VALUE;
-        long yMin = Long.MAX_VALUE;
-
-        for (Point p : points) {
-            if(p.x > xMax) xMax = p.x;
-            if(p.y > yMax) yMax = p.y;
-            if(p.x < xMin) xMin = p.x;
-            if(p.y < yMin) yMin = p.y;
+        if (xUp % down == 0 && yUp % down == 0) {
+            int x = (int)(xUp / down);
+            int y = (int)(yUp / down);
+            return new Point(x, y);
         }
-
-        return new long[]{xMax, yMax, xMin, yMin};
+        return null;
+    }
+    
+    public String[] solution(int[][] line) {
+        List<Point> points = new ArrayList<>();
+        int xMin = Integer.MAX_VALUE;
+        int yMin = Integer.MAX_VALUE;
+        int xMax = Integer.MIN_VALUE;
+        int yMax = Integer.MIN_VALUE;
+        
+        for(int i = 0 ; i < line.length - 1 ; i++) {
+            for(int j = i + 1 ; j < line.length ; j++) {
+                Point point = getPoint(line[i], line[j]);
+                if(point != null) {
+                    points.add(point);
+                    if(point.x > xMax) xMax = point.x;
+                    if(point.x < xMin) xMin = point.x;
+                    if(point.y > yMax) yMax = point.y;
+                    if(point.y < yMin) yMin = point.y;
+                }
+            }
+        }
+        
+        String[][] square = new String[yMax - yMin + 1][xMax - xMin + 1];
+        for(String[] row : square) {
+            Arrays.fill(row, ".");
+        }
+        
+        for(Point point : points) {
+            int x = point.x - xMin;
+            int y = yMax - point.y;
+            square[y][x] = "*";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        String[] answer = new String[square.length];
+        for(int i = 0 ; i < square.length ; i++) {
+            for(String s : square[i]) {
+                sb.append(s);
+            }
+            answer[i] = sb.toString();
+            sb.setLength(0);
+        }
+        
+        return answer;
     }
 }
