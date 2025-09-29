2390f80522e29a8bc1b299d6a89468810d70b693
inwoo <dlsdn1108@gmail.com>
2025-09-30 08:24:35 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.22 ms, Memory: 92 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1 ; i < food.length ; i++) {
+            for(int j = 1 ; j <= food[i] / 2 ; j++) {
+                sb.append(i);
+            }
+        }
+        
+        answer += sb.toString();
+        answer += "0";
+        answer += sb.reverse().toString();
+        
+        return answer;
+    }
+}
