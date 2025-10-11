48baf71e2f768bceab4eb94ca84c15ab39abe80c
inwoo <dlsdn1108@gmail.com>
2025-10-11 15:22:46 +0900

    [level 3] Title: 숫자 게임, Time: 108.96 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int bPointer = 0;
+        
+        for(int i = 0 ; i < A.length ; i++) {
+            while(bPointer < B.length) {
+                if(A[i] < B[bPointer++]) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
