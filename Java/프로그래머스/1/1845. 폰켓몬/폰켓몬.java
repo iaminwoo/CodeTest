723070ba6433d53753594e73541c6b503269da24
inwoo <dlsdn1108@gmail.com>
2025-10-24 14:24:49 +0900

    [level 1] Title: 폰켓몬, Time: 5.94 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        
+        final Set<Integer> set = new HashSet<>();
+        
+        for(int num : nums) {
+            set.add(num);
+        }
+        
+        return Math.min(set.size(), nums.length / 2);
+    }
+}
