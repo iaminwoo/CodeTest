12f6ba873cd7f9e192e24e4b3d518cf4b1811024
inwoo <dlsdn1108@gmail.com>
2025-11-12 17:10:35 +0900

    [level 2] Title: 전화번호 목록, Time: 352.43 ms, Memory: 200 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Set<String> set = new HashSet<>();
+        for(String s : phone_book){
+            set.add(s);
+        }
+        
+        for(String s : phone_book){
+            for(int i = 1 ; i < s.length() ; i++){
+                String prefix = s.substring(0, i);
+                if(set.contains(prefix)) answer = false;
+            }
+        }
+        
+        return answer;
+    }
+}
