81fb8530965b524803d879be017ad375c7e6b13d
inwoo <dlsdn1108@gmail.com>
2025-12-04 11:24:10 +0900

    [level 2] Title: 숫자 변환하기, Time: 91.64 ms, Memory: 107 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        if(x == y) return 0;
+        
+        int answer = 0;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[y + 1];
+        
+        visited[x] = true;
+        queue.add(x);
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for(int i = 0 ; i < size ; i++) {
+                int c = queue.poll();
+            
+                int[] nextNumbers = {c + n, c * 2, c * 3};
+
+                for(int next : nextNumbers) {
+                    if(next > y) continue;
+
+                    if(next == y) return answer + 1;
+
+                    if(!visited[next]) {
+                        visited[next] = true;
+                        queue.add(next);
+                    }
+                }
+            }
+            
+            answer++;
+        }
+        
+        return -1;
+    }
+}
