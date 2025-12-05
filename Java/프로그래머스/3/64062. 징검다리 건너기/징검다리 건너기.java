33f7f17f7d4f471a213784e161c103851dac0e30
inwoo <dlsdn1108@gmail.com>
2025-12-05 14:15:32 +0900

    [level 3] Title: 징검다리 건너기, Time: 70.11 ms, Memory: 96.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        Deque<Integer> deque = new LinkedList<>();
+        int[] maxs = new int[stones.length - k + 1];
+        
+        for(int i = 0 ; i < k ; i++) {
+            while(!deque.isEmpty() && stones[deque.peekLast()] <= stones[i]) {
+                deque.removeLast();
+            }
+            
+            deque.addLast(i);
+        }
+        maxs[0] = stones[deque.peekFirst()];
+        
+        for(int i = 1 ; i <= stones.length - k ; i++) {
+            if(deque.peekFirst() < i) {
+                deque.removeFirst();
+            }
+            
+            int newIdx = i + k - 1;
+            
+            while(!deque.isEmpty() && stones[deque.peekLast()] <= stones[newIdx]) {
+                deque.removeLast();
+            }
+            
+            deque.addLast(newIdx);
+            
+            maxs[i] = stones[deque.peekFirst()];
+        }
+        
+        int answer = Integer.MAX_VALUE;
+        for(int m : maxs) {
+            answer = Math.min(m, answer);
+        }
+        
+        return answer;
+    }
+}
