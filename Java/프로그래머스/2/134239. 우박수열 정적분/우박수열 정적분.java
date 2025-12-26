cd826a92fd3d6ea72d3248bdf7f126537b33aec5
inwoo <dlsdn1108@gmail.com>
2025-12-26 11:38:35 +0900

    [level 2] Title: 우박수열 정적분, Time: 0.85 ms, Memory: 106 MB -BaekjoonHub

--- Java/프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ Java/프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public double[] solution(int k, int[][] ranges) {
         double[] answer = new double[ranges.length];
@@ -11,6 +9,7 @@ class Solution {
         while (k > 1) {
             int temp = k;
             count++;
+            idx++;
             
             if (k % 2 == 0) {
                 k = k / 2;
@@ -18,26 +17,28 @@ class Solution {
                 k = k * 3 + 1;
             }
             
-            dp[idx++] = (double) (temp + k) / 2;
+            if (count == 1) {
+                dp[idx] = (double) (temp + k) / 2;
+            } else {
+                dp[idx] = dp[idx - 1] + (double) (temp + k) / 2;
+            }
         }
         
-        int i = 0;
+        for (int i = count ; i < 200 ; i++) {
+            dp[i] = dp[count];
+        }
+        
+        int j = 0;
         for (int[] r : ranges) {
             int a = r[0];
             int b = count + r[1];
             
             if (a > b) {
-                answer[i++] = -1;
+                answer[j++] = -1;
                 continue;
             }
             
-            double sum = 0;
-                        
-            for (int j = a ; j < b ; j++) {
-                sum += dp[j];
-            }
-            
-            answer[i++] = sum;
+            answer[j++] = dp[b] - dp[a];
         }
         
         return answer;
