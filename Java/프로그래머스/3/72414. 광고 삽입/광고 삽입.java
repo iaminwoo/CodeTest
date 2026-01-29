1373fa8fbf0e447197aa723b6ca672079eea3e0b
inwoo <dlsdn1108@gmail.com>
2026-01-29 10:12:57 +0900

    [level 3] Title: 광고 삽입, Time: 359.00 ms, Memory: 295 MB -BaekjoonHub

--- Java/프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ Java/프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,91 +1,68 @@
-import java.util.*;
-
 class Solution {
-    private int[] videoTime;
-    private int playTimeSecond;
-    private int advTime;
-    
-    private void logALog(String log) {
-        String[] parts = log.split("-");
-        int start = timeToSecond(parts[0]);
-        int end = timeToSecond(parts[1]);
-        
-        videoTime[start]++;
-        videoTime[end]--;
-    }
     
     private int timeToSecond(String time) {
         String[] parts = time.split(":");
+        
         int second = 0;
-        for (int i = 0 ; i < 3 ; i++) {
-            second += Integer.parseInt(parts[i]) * Math.pow(60, 2 - i);
+        for (int i = 0 ; i <= 2; i++) {
+            second += (Integer.parseInt(parts[i]) * (int)Math.pow(60, 2 - i));
         }
         return second;
     }
     
-    private void checkViewers() {
-        int curr = 0;
-        
-        for (int i = 0 ; i < videoTime.length ; i++) {
-            curr += videoTime[i];
-            videoTime[i] = curr;
-        }
-    }
-    
-    private String secondToTime(int second) {
+    private String secondToTime(long second) {
         String time = "";
+        
         for (int i = 2 ; i >= 0 ; i--) {
-            int pow = (int) Math.pow(60, i);
-            String s = second / pow >= 10 ? "" + second / pow : "0" + second / pow;
-            time += s;
-            if (i > 0) time += ":";
-            second = second % pow;
+            if (i < 2) time += ":";
+            int timeSecond = (int)Math.pow(60, i);
+            if (second / timeSecond < 10) time += "0";
+            time += second / timeSecond;
+            second = second % timeSecond;
         }
         return time;
     }
     
-    private String getMaxStart() {
-        long maxWatchTime = 0;
-        int maxStart = 0;
+    
+    public String solution(String play_time, String adv_time, String[] logs) {
         
-        long watchTime = 0;
-        for (int i = 0 ; i < advTime ; i++) {
-            watchTime += (long)videoTime[i];
+        int video_length = timeToSecond(play_time);
+        int[] video = new int[video_length + 1];
+        
+        for (String log : logs) {
+            String[] parts = log.split("-");
+            String start = parts[0];
+            String end = parts[1];
+            
+            video[timeToSecond(start)]++;
+            video[timeToSecond(end)]--;
         }
         
-        int start = 0;
-        int end = advTime - 1;
-        
-        if (maxWatchTime < watchTime) {
-            maxWatchTime = watchTime;
-            maxStart = start;
+        int viewer = 0;
+        for (int i = 0 ; i < video.length ; i++) {
+            viewer = viewer + video[i];
+            video[i] = viewer;
         }
         
-        while (end < playTimeSecond) {
-            watchTime -= (long)videoTime[start++];
-            watchTime += (long)videoTime[++end];
-            if (maxWatchTime < watchTime) {
-                maxWatchTime = watchTime;
-                maxStart = start;
+        int adv_length = timeToSecond(adv_time);
+        
+        long totalView = 0;
+        for (int i = 0 ; i < adv_length ; i++) {
+            totalView += video[i];
+        }
+        
+        long maxTotalView = totalView;
+        int startSecond = 0;
+        
+        for (int adv_start = 0 ; adv_start < video_length - adv_length ; adv_start++) {
+            totalView -= video[adv_start];
+            totalView += video[adv_start + adv_length];
+            if (totalView > maxTotalView) {
+                startSecond = adv_start + 1;
+                maxTotalView = totalView;
             }
         }
         
-        return secondToTime(maxStart);
-    }
-    
-    public String solution(String play_time, String adv_time, String[] logs) {
-        String answer = "";
-        
-        playTimeSecond = timeToSecond(play_time);
-        advTime = timeToSecond(adv_time);
-        videoTime = new int[playTimeSecond + 1];
-        
-        for (String log : logs) {
-            logALog(log);
-        }
-        
-        checkViewers();
-        
-        return getMaxStart();
+        return secondToTime(startSecond);
     }
 }
