ac962887a4df304b3934d31f27bdf83b7e941b67
inwoo <dlsdn1108@gmail.com>
2026-02-02 10:55:26 +0900

    [level 2] Title: 가장 큰 수, Time: 251.58 ms, Memory: 141 MB -BaekjoonHub

--- Java/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ Java/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,12 +1,22 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     public String solution(int[] numbers) {
-        return Arrays.stream(numbers)
-                .mapToObj(String::valueOf)
-                .sorted((s1, s2) -> (s2+s1).compareTo(s1+s2))
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                .toString()
-                .replaceAll("^0+", "0");
+        
+        String[] stringArr = new String[numbers.length];
+        
+        for (int i = 0 ; i < numbers.length ; i++) {
+            stringArr[i] = "" + numbers[i];
+        }
+        
+        Arrays.sort(stringArr, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (stringArr[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        for (String s : stringArr) {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 }
