a25445af34bb8048980ab07c488db0524de689fc
inwoo <dlsdn1108@gmail.com>
2026-03-11 09:10:47 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.23 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(my_string, index_list) {
+    let answer = '';
+    
+    for (let index of index_list) {
+        answer += my_string.charAt(index);
+    }
+    
+    return answer;
+}
