acaefefb7beca1f11a5042f48aef6b1a14d18fe3
inwoo <dlsdn1108@gmail.com>
2026-03-16 14:31:10 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ JavaScript/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,5 +1,4 @@
 function solution(myString, pat) {
-    const newString = myString.replaceAll("A", "b").replaceAll("B", "a");
-    const newPat = pat.replaceAll("A", "a").replaceAll("B", "b");
-    return newString.includes(newPat) ? 1 : 0;
+    const converted = [...myString].map(c => c === "A" ? "B" : "A").join("");
+    return converted.includes(pat) ? 1 : 0;
 }
