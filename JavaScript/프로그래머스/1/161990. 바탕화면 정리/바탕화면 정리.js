a3b61a333f7275ecb7f98dddef136fb5fcf16468
inwoo <dlsdn1108@gmail.com>
2026-03-16 09:54:38 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ JavaScript/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,64 +1,15 @@
 function solution(wallpaper) {
-    let answer = [];
+    const rows = [];
+    const cols = [];
     
-    const m = wallpaper.length;
-    const n = wallpaper[0].length;
-    
-    let lux = 0;
-    for (let i = 0 ; i < m ; i++) {
-        let found = false;
-        for (let j = 0 ; j < n ; j++) {
-            if (wallpaper[i][j] === "#") {
-                found = true;
-                lux = i;
-                break;
+    wallpaper.forEach((row, i) => {
+        [...row].forEach((char, j) => {
+            if (char === "#") {
+                rows.push(i);
+                cols.push(j);
             }
-        }
-        if (found) break;
-    }
-    answer[0] = lux;
+        });
+    });
     
-    let luy = 0;
-    for (let i = 0 ; i < n ; i++) {
-        let found = false;
-        for (let j = 0 ; j < m ; j++) {
-            if (wallpaper[j][i] === "#") {
-                found = true;
-                luy = i;
-                break;
-            }
-        }
-        if (found) break;
-    }
-    answer[1] = luy;
-    
-    let rdx = 0;
-    for (let i = m - 1 ; i >= 0 ; i--) {
-        let found = false;
-        for (let j = n - 1 ; j >= 0 ; j--) {
-            if (wallpaper[i][j] === "#") {
-                found = true;
-                rdx = i;
-                break;
-            }
-        }
-        if (found) break;
-    }
-    answer[2] = rdx + 1;
-    
-    let rdy = 0;
-    for (let i = n - 1 ; i >= 0 ; i--) {
-        let found = false;
-        for (let j = m - 1 ; j >= 0 ; j--) {
-            if (wallpaper[j][i] === "#") {
-                found = true;
-                rdy = i;
-                break;
-            }
-        }
-        if (found) break;
-    }
-    answer[3] = rdy + 1;
-    
-    return answer;
+    return [Math.min(...rows), Math.min(...cols), Math.max(...rows) + 1, Math.max(...cols) + 1];
 }
