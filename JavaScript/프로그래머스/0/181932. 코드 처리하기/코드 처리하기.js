01929dacd3936e4fe241c7aa1e4f3970eeacf276
inwoo <dlsdn1108@gmail.com>
2026-03-19 09:34:00 +0900

    [level 0] Title: 코드 처리하기, Time: 24.48 ms, Memory: 38.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,18 +1,4 @@
 function solution(code) {
-    let mode = 0;
-    let ret = '';
-    
-    for (let i = 0 ; i < code.length ; i++) {
-        if (code[i] === "1") {
-            mode = mode === 1 ? 0 : 1;
-        } else if (mode === 0) {
-            if (i % 2 === 0) ret += code[i];
-        } else {
-            if (i % 2 !== 0) ret += code[i];
-        }
-    }
-    
-    if (ret.length === 0) ret = "EMPTY";
-    
-    return ret;
+    let ret = [...code.replaceAll("1","")].filter((c, i) => i % 2 === 0).join("");  
+    return ret.length === 0 ? "EMPTY" : ret;
 }
